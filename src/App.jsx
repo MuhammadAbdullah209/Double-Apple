3d4524dffeb38077f190f9152287b6c44aeff480
M.Abdullah <abdullah488838@gmail.com>
2026-09-02 17:01:58 +0500

    Scrolling-issue-fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
+import ScrollToTop from './components/ScrollToTop'
 import Home from './pages/Home'
 import CreateAccount from './pages/CreateAccount'
 import SignIn from './pages/SignIn'
@@ -14,21 +15,24 @@ import Profile from './pages/Profile'
 
 function App() {
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/create-account" element={<CreateAccount />} />
-        <Route path="/create-account/verify" element={<VerifyOtp />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/shop/:slug" element={<ProductDetail />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/about-us" element={<AboutUs />} />
-        <Route path="/contact-us" element={<ContactUs />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/profile" element={<Profile />} />
-      </Route>
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/create-account" element={<CreateAccount />} />
+          <Route path="/create-account/verify" element={<VerifyOtp />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/shop/:slug" element={<ProductDetail />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/about-us" element={<AboutUs />} />
+          <Route path="/contact-us" element={<ContactUs />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+      </Routes>
+    </>
   )
 }
 
